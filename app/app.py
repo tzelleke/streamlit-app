baf9480e2b043931326c435996d16fe598b5c420
tzelleke <theodros.zelleke@gmail.com>
2023-03-15 14:44:28 +0100

    feat: demo aggrid

--- app/app.py
+++ app/app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+from st_aggrid import AgGrid
+import pandas as pd
 
-st.text('Hello World!')
+URL = 'https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv'
+
+st.set_page_config(layout="wide")
+st.title('Flight data analysis')
+
+df = pd.read_csv(URL)
+AgGrid(df, height=400)
